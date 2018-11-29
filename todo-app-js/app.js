13e06a9360632962c1603d99e66cb196abce363b
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-11-29 14:06:40 +0000

    added todo app in vanilla js

---
+++ todo-app-js/app.js
@@ -0,0 +1,42 @@
+const renderTodo = (todo, index) => {
+  const todoHTML = `<li id="item-${index}"><span class="label">${todo}</span><span class="actions"><button type="button" aria-label="Delete" title="Delete" class="btn-picto"><i class="fas fa-trash" id="${index}"></i></button></span></li>`;
+  const todosList = document.querySelector('#todos ul');
+
+  todosList.insertAdjacentHTML('afterbegin', todoHTML);
+};
+
+const deleteTodo = (index) => {
+  todos.splice(index);
+  const item = document.getElementById(`item-${index}`);
+  item.remove();
+};
+
+const addDeleteEvent = (element) => {
+  const deleteButton = element.querySelector('button');
+  deleteButton.addEventListener('click', (event) => deleteTodo(event.target.id));
+};
+
+const todos = ['Code a todo app'];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log('App running...')
+    todos.forEach((todo, index) => renderTodo(todo, index));
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const todo = form.querySelector('#newitem').value;
+      if (todo != '') {
+        form.reset();
+        todos.push(todo);
+        renderTodo(todo);
+        addDeleteEvent(document.querySelector('#todos ul').firstChild);
+      }
+    });
+
+    const todoItems = document.querySelectorAll('#todos ul li');
+    todoItems.forEach((item) => {
+      const deleteButton = item.querySelector('button');
+      deleteButton.addEventListener('click', (event) => deleteTodo(event.target.id));
+    })
+});
